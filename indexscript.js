301373cb07407f7077829403220843563c4ae78b
MEHMETCERANX12 <ceranmehmet016@gmail.com>
2024-09-09 10:46:49 +0300

    Create indexscript.js

---
+++ indexscript.js
@@ -0,0 +1,9 @@
+        function loginhata()
+        {
+        var element = document.getElementById("mesaj");
+        element.style.display = "flex";
+            setTimeout(function ()
+            {
+            element.style.display = "none";
+        }, 7000);
+        }
