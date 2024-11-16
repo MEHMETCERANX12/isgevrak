6ccd0c4ae510e04b854cc772d8db5a1748225443
MEHMETCERANX12 <ceranmehmet016@gmail.com>
2024-11-16 17:37:48 +0300

    Update and rename isgevrak1_3.js to isgevrak1_4.js

--- isgevrak1_4.js
+++ isgevrak1_4.js
@@ -171,7 +171,24 @@ $(document).ready(function ()
         });
 
 
-
+$(function() {
+    $(".csstexboxtarih").datepicker({
+        firstDay: 1,
+        dateFormat: "dd.mm.yy",
+        autoSize: false,
+        changeMonth: true,
+        changeYear: true,
+        dayNames: ["pazar", "pazartesi", "salı", "çarşamba", "perşembe", "cuma", "cumartesi"],
+        dayNamesMin: ["paz", "pzt", "sal", "çar", "per", "cum", "cmt"],
+        defaultDate: 0,
+        maxDate: "+1y+0m+0w",
+        minDate: "-6y-0m-0w",
+        monthNamesShort: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+        nextText: "ileri",
+        prevText: "geri",
+        showAnim: "slideDown"
+    });
+});
 
 
 
